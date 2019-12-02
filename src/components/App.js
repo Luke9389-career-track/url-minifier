47ee9ac2bbd5056a096b1cdcfab3aa4cebb212a9
Luke Donahue <luke9389@gmail.com>
2019-12-02 15:26:46 -0800

    set up react slice with users

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Switch,
+  Route
 } from 'react-router-dom';
 import Header from './Header';
+import SignupUser from '../containers/SignupUser';
+import LoginUser from '../containers/LoginUser';
 
 export default function App() {
   return (
     <Router>
       <Header />
+      <Switch>
+        <Route path="/login" component={LoginUser} />
+        <Route path="/signup" component={SignupUser} />
+      </Switch>
     </Router>
   );
 }
